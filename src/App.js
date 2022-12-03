405b0e927f0464c326b6a8b043a9bda3aa117831
luis <6010ldaniel@gmail.com>
2022-12-03 18:50:53 -0300

    README.md

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import './App.css';
+
+function handleOnMouseOver(event) {
+  console.log(event)
+  console.log("Pass!")
+}
+
+function handleClick(event) {
+  console.log(event)
+  console.log("Clicado")
+}
+
+function App() {
+  return (
+    <div>
+      <div onMouseOver={handleOnMouseOver}>Pase o mouse por aqui!!</div>
+      <button onClick={handleClick}> Me clique</button>
+
+    </div>
+  );
+}
+
+export default App;*/
